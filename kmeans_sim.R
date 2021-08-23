fcaca3dc474495e7eb7a9226e3773bcc0d4c77f0
Parker Knight <plb.knight@gmail.com>
2021-08-23 08:37:34 -0400

    family kmeans sim

--- kmeans_sim.R
+++ kmeans_sim.R
@@ -36,3 +36,19 @@ kmeans.out <- kmeans(X, centers = 2)
 qplot(svd(X)$u[,1], svd(X)$u[,2],
       col = factor(kmeans.out$cluster)) +
   theme_classic()
+
+##### 2 groups, "family" correlated samples, low dim
+n <- 100
+p <- 50
+rho <- 0.5
+Sigma = bdiag(lapply(1:(n/5), function(i){
+ I <- (1-rho) * diag(5)
+ ones <- rho * rep(1 ,5) %*% t(rep(1, 5))
+ I + ones
+}))
+X <- t(mvrnorm(n = p, mu = c(rep(1, n/2), rep(0,n/2)),
+        Sigma = Sigma))
+kmeans.out <- kmeans(X, centers = 2)
+qplot(svd(X)$u[,1], svd(X)$u[,2],
+      col = factor(kmeans.out$cluster)) +
+  theme_classic()
